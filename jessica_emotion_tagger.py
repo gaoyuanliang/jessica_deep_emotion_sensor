f4c1009bf75b151191eb6a3a6041b36f671704af
Jessica Gaoyuan Liang <gaoyuanliang@outlook.com>
2021-01-03 23:18:18 +0400

    Update jessica_emotion_tagger.py

--- jessica_emotion_tagger.py
+++ jessica_emotion_tagger.py
@@ -7,10 +7,10 @@ from tensorflow.keras.utils import *
 
 from jessica_deep_emotion_sensor import texts_to_input
 
-fear_tagger = keras.models.load_model('/Downloads/fear_tagger.h5')
-sadness_tagger = keras.models.load_model('/Downloads/sadness_tagger.h5')
-joy_tagger = keras.models.load_model('/Downloads/joy_tagger.h5')
-anger_tagger = keras.models.load_model('/Downloads/anger_tagger.h5')
+fear_tagger = keras.models.load_model('fear_tagger.h5')
+sadness_tagger = keras.models.load_model('sadness_tagger.h5')
+joy_tagger = keras.models.load_model('joy_tagger.h5')
+anger_tagger = keras.models.load_model('anger_tagger.h5')
 
 tagger_models = {"fear": fear_tagger,
 "sadness": sadness_tagger,
@@ -29,19 +29,4 @@ def emotion_tagging(text):
 			output_tags.append({'tag':emotion, "confidence":confidence})
 	return output_tags
 
-'''
-from jessica_emotion_tagger import emotion_tagging
-
-emotion_tagging(u"All I want to do is watch some netflix but I am stuck here in class. #depressing")
-#[{'tag': 'sadness', 'confidence': 0.99995244}]
-
-emotion_tagging(u"Watching It Follows.  This is a super freaky movie.  #scary")
-#[{'tag': 'fear', 'confidence': 0.9999993}]
-
-emotion_tagging(u"Ready for that nice, breezy, calm, sunshine weather.🍂🍁 #Autumn")
-#[{'tag': 'joy', 'confidence': 0.9989052}]
-
-emotion_tagging(u"@leesyatt you are a cruel, cruel man. #therewillbeblood #revenge")
-#[{'tag': 'anger', 'confidence': 1.0}]
-'''
 ########jessica_emotion_tagger.py########
