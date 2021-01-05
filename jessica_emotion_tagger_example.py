06eebce125394df74c1fc49c131feaf667b09e19
Jessica Gaoyuan Liang <gaoyuanliang@outlook.com>
2021-01-05 23:01:52 +0400

    Update jessica_emotion_tagger_example.py

--- jessica_emotion_tagger_example.py
+++ jessica_emotion_tagger_example.py
@@ -1,13 +1,9 @@
 from jessica_emotion_tagger import emotion_tagging
 
 emotion_tagging(u"All I want to do is watch some netflix but I am stuck here in class. #depressing")
-#[{'tag': 'sadness', 'confidence': 0.99995244}]
 
 emotion_tagging(u"Watching It Follows.  This is a super freaky movie.  #scary")
-#[{'tag': 'fear', 'confidence': 0.9999993}]
 
 emotion_tagging(u"Ready for that nice, breezy, calm, sunshine weather.🍂🍁 #Autumn")
-#[{'tag': 'joy', 'confidence': 0.9989052}]
 
 emotion_tagging(u"@leesyatt you are a cruel, cruel man. #therewillbeblood #revenge")
-#[{'tag': 'anger', 'confidence': 1.0}]
